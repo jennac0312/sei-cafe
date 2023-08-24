7429f345ecd52d64acfa1b0d49e45214ba8d0fd7
Jenna C <jennac0312@gmail.com>
2023-08-24 15:44:06 -0400

    more typos of course

--- src/components/SignUpForm/SignUpForm.jsx
+++ src/components/SignUpForm/SignUpForm.jsx
@@ -24,7 +24,7 @@ export default class SignUpForm extends Component {
     // in summary, in class components, use arrow functions for automatic binding
     
     handleChange = ( evt ) => {
-        // setState funciton inheritied form component - sola flames thanks
+        // setState function inherited from component - sola flames thanks
         this.setState({
             [evt.target.name] : evt.target.value,
             // dynamic way of updating our state depending on the event object that we're currently on
