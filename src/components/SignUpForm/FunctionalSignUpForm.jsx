b1ea8d9c7e485565b9be2e4c411a35529e87113d
Jenna C <jennac0312@gmail.com>
2023-08-25 11:33:25 -0400

    setting up handleSubmit. console logs correct info

--- src/components/SignUpForm/FunctionalSignUpForm.jsx
+++ src/components/SignUpForm/FunctionalSignUpForm.jsx
@@ -2,26 +2,42 @@ import { useEffect, useState } from "react"
 
 const FunctionalSignUpForm = () => {
 
-    const [ state, setState ] = useState({})
+    const [ formData, setFormData ] = useState({})
 
     // let disable
 
     // useEffect(() => {
-       const disable = state.password !== state.confirm;
+       const disable = formData.password !== formData.confirm;
     // }, [state.password])
 
 
     const handleChange = ( evt ) => {
-        setState({
-            ...state,
+        setFormData({
+            ...formData,
             [evt.target.name] : evt.target.value,
             error: ""
         })
     }
 
-    const handleSubmit = ( evt ) => {
-        evt.preventDefault()
-        alert( JSON.stringify(state) )
+    const handleSubmit = async ( evt ) => {
+        evt.preventDefault() // prevent form from being submitted to the server. ( sleight of hand that is SPA... interrupting before server )
+        // alert( JSON.stringify(state) )
+            // alerts are a big no no. they are aggressive. dont use them
+
+            try {
+                // what dont we want to send to server? error and confirm
+                // why? they're not necessary, wont be part of our models
+                // so let's make a copy of the state object, then delete them
+                const newFormData = {...formData};
+                delete newFormData.error;
+                delete newFormData.confirm;
+                
+                console.log( 'inside handlesubmit', newFormData )
+
+            } catch (error) {
+                setFormData( {error: "Sign Up Failed. Try Again"} )
+            }
+        
     }
 
   return (
@@ -29,17 +45,17 @@ const FunctionalSignUpForm = () => {
                 <div className="form-container">
                 <form autoComplete="off" onSubmit={handleSubmit}>
                     <label>Name</label>
-                    <input type="text" name="name" value={state.name} onChange={handleChange} required />
+                    <input type="text" name="name" value={formData.name} onChange={handleChange} required />
                     <label>Email</label>
-                    <input type="email" name="email" value={state.email} onChange={handleChange} required />
+                    <input type="email" name="email" value={formData.email} onChange={handleChange} required />
                     <label>Password</label>
-                    <input type="password" name="password" value={state.password} onChange={handleChange} required />
+                    <input type="password" name="password" value={formData.password} onChange={handleChange} required />
                     <label>Confirm</label>
-                    <input type="password" name="confirm" value={state.confirm} onChange={handleChange} required />
+                    <input type="password" name="confirm" value={formData.confirm} onChange={handleChange} required />
                     <button type="submit" disabled={disable}>SIGN UP</button>
                 </form>
                 </div>
-            <p className="error-message">&nbsp;{state.error}</p>
+            <p className="error-message">&nbsp;{formData.error}</p>
           </div>
   )
 }
